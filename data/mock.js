014dc8b7935f6212af41ef930e6dbff1e72b9420
shfshanyue <xianger94@gmail.com>
2022-05-16 17:37:36 +0800

    update

--- data/mock.js
+++ data/mock.js
@@ -5,8 +5,12 @@ const sidebar = [
     collapsable: true,
     children: [
       {
-        title: '043. 四年广州',
+        title: '043. 四年广州期望21',
         path: '220510'
+      },
+      {
+        title: '044. 两年广州期望14',
+        path: '220511'
       }
     ]
   },
